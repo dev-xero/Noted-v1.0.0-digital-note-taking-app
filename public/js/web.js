f5da01f4cbe1903da73b4549edf3f52b38e63369
CodeNinja-tech <elo48858@gmail.com>
2022-08-16 22:44:43 +0100

    finished the review section

---
+++ public/js/web.js
@@ -0,0 +1,7 @@
+const AVATAR_URL = "/public/assets/images/avatars/avatar-001.png"
+
+const avatarImg = document.querySelector('.review-avatar')
+
+document.addEventListener('DOMContentLoaded', () => {
+	avatarImg.setAttribute('src', AVATAR_URL)
+}, false)
